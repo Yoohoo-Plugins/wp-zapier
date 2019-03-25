f427537cacee4d4322fe6e41a0439a19634e7f27
Andrew Lima <andrew@arctek.co.za>
2019-03-25 22:18:15 +0200

    Settings Header Updates
    Settings Header updates and improved styling.

--- includes/settings-header.php
+++ includes/settings-header.php
@@ -3,14 +3,24 @@
  * Setting header for admin area.
  */
 ?>
-<h1><?php _e( 'Zapier Settings', 'wp-zapier' ); ?></h1>
-<hr/>
-<style>
-	.wp-zapier-action-links { margin-top: 20px; }
-	.wp-zapier-action-links a { text-decoration:none; }
-	.wp-zapier-action-links a.active { color: black; font-weight:bold; }
-</style>
-<div class='wp-zapier-action-links'>
-	<a <?php if ( ! isset( $_REQUEST['receive_data'] ) && ! isset( $_REQUEST['license_settings'] ) ) { echo "class='active'"; } ?> href="<?php echo admin_url( 'options-general.php?page=wp-zapier-settings' ); ?>"><?php _e( 'Send Data To Zapier', 'wp-zapier' ); ?></a> | <a <?php if ( isset( $_REQUEST['receive_data'] ) ) { echo "class='active'"; } ?> href="<?php echo admin_url( 'options-general.php?page=wp-zapier-settings&receive_data=true' ); ?>"><?php _e( 'Receive Data From Zapier', 'wp-zapier' ); ?></a> | <a <?php if ( isset( $_REQUEST['license_settings'] ) ) { echo "class='active'"; } ?> href="<?php echo admin_url( 'options-general.php?page=wp-zapier-settings&license_settings=1' ); ?>"><?php _e( 'License Settings', 'wp-zapier' ); ?></a>
+<div class="wp-zapier-settings-header" style="font-size:25px;margin:2% 0 0 0;display:inline-block;">
+	<?php _e( 'WP Zapier - Settings', 'wp-zapier' );?>
+	<div class="wp-zapier-settings-header-meta" style="font-size:14px;margin-left:5px;display:inline-block;">
+		<?php echo "v" . WPZP_VERSION; ?>
+		<span style="margin-left:5px;"><a href="#" class="button" style="margin-top:-4px;"><?php _e( 'Documentation', 'wp-zapier' ); ?></a></span>
+		<span style="margin-left:5px;"><a href="#" class="button button-primary" style="margin-top:-4px;"><?php _e( 'Support', 'wp-zapier' ); ?></a></span>
+	</div>
 </div>
+
+<div class="wrap"><h2 style="margin:0;font-size:1px;"></h2></div>
+
+<nav class='wp-zapier-action-links nav-tab-wrapper'>
+	<a href="<?php echo admin_url( 'options-general.php?page=wp-zapier-settings' ); ?>" class="nav-tab <?php if( ! isset( $_REQUEST['receive_data'] ) && ! isset( $_REQUEST['license_settings'] ) ) { echo "nav-tab-active"; } ?>"><?php _e( 'Send Data To Zapier', 'wp-zapier' ); ?></a>
+
+	<a href="<?php echo admin_url( 'options-general.php?page=wp-zapier-settings&receive_data=true' ); ?>" class="nav-tab <?php if( isset( $_REQUEST['receive_data'] ) && $_REQUEST['receive_data'] == 'true' ) { echo "nav-tab-active"; } ?>"><?php _e( 'Receive Data From Zapier', 'wp-zapier' ); ?></a>
+
+	<a href="<?php echo admin_url( 'options-general.php?page=wp-zapier-settings&license_settings=true' ); ?>" class="nav-tab <?php if( isset( $_REQUEST['license_settings'] ) && $_REQUEST['license_settings'] == 'true' ) { echo "nav-tab-active"; } ?>"><?php _e( 'License Settings', 'wp-zapier' ); ?></a>
+
+	<?php do_action( 'wp_zapier_settings_header_tabs' ); ?>
+</nav>
 <br/>
