00f30a43dd872ed7a9875054b5035224e3e92aba
Andrew Lima <andrew@arctek.co.za>
2022-05-16 20:58:23 +0200

    Adjust plugin name in settings header.

--- includes/settings-header.php
+++ includes/settings-header.php
@@ -4,7 +4,7 @@
  */
 ?>
 <div class="wp-zapier-settings-header" style="font-size:25px;margin:2% 0 0 0;display:inline-block;">
-	<?php _e( 'WP Zapier - Settings', 'wp-zapier' );?>
+	<?php _e( 'WP Zapier Integration - Settings', 'wp-zapier' );?>
 	<div class="wp-zapier-settings-header-meta" style="font-size:14px;margin-left:5px;display:inline-block;">
 		<?php echo "v" . WPZAP_VERSION; ?>
 		<span style="margin-left:5px;"><a href="https://yoohooplugins.com/?s=zapier" style="margin-top:-4px;" target="_blank"><?php esc_html_e( 'Documentation', 'wp-zapier' ); ?></a> | <a href="https://yoohooplugins.com/support" target="_blank" style="margin-top:-4px;"><?php esc_html_e( 'Support', 'wp-zapier' ); ?></a></span>
